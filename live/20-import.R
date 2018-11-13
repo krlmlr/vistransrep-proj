abeb766edcfd5c7419efeedb3dfdafe0b47d8279
Kirill Müller <krlmlr+r@mailbox.org>
2018-11-13 14:43:10 +0000

    import

---
+++ live/20-import.R
@@ -0,0 +1,18 @@
+library(readr)
+library(here)
+
+example1 <- read_delim(here("data/example1.csv"),
+                       ";",
+                       escape_double = FALSE,
+                       trim_ws = TRUE)
+View(example1)
+
+example1 <- read_delim(here("data/example1.csv"),
+                       ";",
+                       col_types = cols(
+                         col1 = col_double(),
+                         col2 = col_character(),
+                         col3 = col_character()
+                       ),
+                       escape_double = FALSE,
+                       trim_ws = TRUE)
