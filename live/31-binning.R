7581092ae2ea3afef9139cd1cf18b7484e7e9b1a
Kirill Müller <krlmlr@mailbox.org>
2018-12-05 14:45:44 +0000

    changes from cd96d1d11c38 on Wed Dec 5 14:45:44 UTC 2018

--- live/31-binning.R
+++ live/31-binning.R
@@ -8,3 +8,19 @@ flights %>%
       quantile(distance, c(.25, .5, .75))
     )
   )
+
+flights %>%
+  summarize(
+    quartiles = list(
+      quantile(distance, c(.25, .5, .75))
+    )
+  ) %>%
+  pull()
+
+flights %>%
+  mutate(
+    distance_class =
+      quantile(distance, c(.25, .5, .75))
+    )
+  ) %>%
+  pull()
