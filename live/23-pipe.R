a7e48ec16539e91ff46f9fb42275e63ebf10418d
Kirill Müller <krlmlr@mailbox.org>
2018-12-05 13:07:11 +0000

    changes from cd96d1d11c38 on Wed Dec 5 13:07:11 UTC 2018

--- live/23-pipe.R
+++ live/23-pipe.R
@@ -11,3 +11,9 @@ flights %>%
   View()
 
 View(arrange(flights, dep_time, sched_dep_time))
+
+flights %>%
+  filter(carrier == "UA") %>%
+  arrange(arr_delay)
+
+arrange(filter(flights, carrier == "UA"), arr_delay)
