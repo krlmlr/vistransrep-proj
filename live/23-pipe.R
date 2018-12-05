d722cc5fb1ba95e848f652f3c4c6c638c6679ae1
Kirill Müller <krlmlr@mailbox.org>
2018-12-05 13:12:51 +0000

    changes from cd96d1d11c38 on Wed Dec 5 13:12:51 UTC 2018

--- live/23-pipe.R
+++ live/23-pipe.R
@@ -43,3 +43,9 @@ ua_flights <- filter(flights, carrier == "UA")
 non_hnl_ua_flights <- filter(ua_flights, carrier == "UA")
 #early_ua_flights <- filter(ua_flights, dep_time < 800)
 early_ua_flights <- filter(non_hnl_ua_flights, dep_time < 800)
+
+. <- flights
+. <- filter(flights, carrier == "UA")
+non_hnl_ua_flights <- filter(ua_flights, carrier == "UA")
+#early_ua_flights <- filter(ua_flights, dep_time < 800)
+early_ua_flights <- filter(non_hnl_ua_flights, dep_time < 800)
