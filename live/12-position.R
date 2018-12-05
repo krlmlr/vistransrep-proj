e5d4893ace3e84587c6b59dfb7c6a3c1b69054b5
Kirill Müller <krlmlr@mailbox.org>
2018-12-05 10:59:48 +0000

    changes from 9cfeda8529d9 on Wed Dec 5 10:59:48 UTC 2018

--- live/12-position.R
+++ live/12-position.R
@@ -3,5 +3,13 @@ library(tidyverse)
 ggplot(data = mpg) +
   geom_histogram(
     mapping = aes(x = hwy, fill = drv),
-    binwidth = 1
+    binwidth = 1,
+    alpha = 0.2
+  )
+
+ggplot(data = mpg) +
+  geom_histogram(
+    mapping = aes(x = hwy, fill = drv),
+    binwidth = 1,
+    position = "dodge"
   )
