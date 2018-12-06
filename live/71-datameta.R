9bbf8e8e20ec9b635beb58b2f730ebfc9fe29c6e
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 12:20:17 +0000

    changes from cd96d1d11c38 on Thu Dec 6 12:20:17 UTC 2018

--- live/71-datameta.R
+++ live/71-datameta.R
@@ -5,20 +5,18 @@ library(dataMeta)
 data("esoph")
 my.data <- esoph
 
-# Add description for each variable names and variable type
-variable_description <- c("age group", "alcohol consumption", "tobacco consumption",
-                          "number of cases", "number of controls")
+# Linker: Add description for each variable names and variable type
+variable_description <- c("age group in years", "alcohol consumption in gm/day",
+                          "tobacco consumption in gm/day", "number of cases (showing a range)",
+                          "number of controls (showing range)")
 
 variable_type <- c(0, 0, 0, 0, 0)
-
 linker <- build_linker(my.data = my.data, variable_description = variable_description,
                        variable_type = variable_type)
 linker
 
-## Not run:
-variable_description <- c("age group", "alcohol consumption", "tobacco consumption",
-                          "number of cases", "number of controls")
-variable_type <- c(0, 2, 0, 0, 0)
-linker <- build_linker(my.data = my.data, variable_description = variable_description,
-                       variable_type = variable_type)
-linker
+# Data dictionary
+# For this data set, no further option description is needed.
+dictionary <- build_dict(my.data = my.data, linker = linker, option_description = NULL,
+                         prompt_varopts = FALSE)
+dictionary
