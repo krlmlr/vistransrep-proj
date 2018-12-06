acec00f1f9937cc0d35b781a91fcb5fdced026fb
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 09:32:41 +0000

    changes from cd96d1d11c38 on Thu Dec 6 09:32:41 UTC 2018

--- live/42-two-ds.R
+++ live/42-two-ds.R
@@ -6,8 +6,10 @@ table4b
 
 table4a_long <-
   table4a %>%
-  gather(year, count, -country)
+  gather(year, cases, -country)
 
 table4b_long <-
   table4b %>%
-  gather(year, count, -country)
+  gather(year, populaton, -country)
+
+left_join(table4a, table4b)
