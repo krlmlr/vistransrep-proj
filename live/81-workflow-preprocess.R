8e0088d341fd3355db5483c7f0a90b2f8b22c774
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 12:46:07 +0000

    changes from cd96d1d11c38 on Thu Dec 6 12:46:06 UTC 2018

--- live/81-workflow-preprocess.R
+++ live/81-workflow-preprocess.R
@@ -1,2 +1,18 @@
 library(tidyverse)
 
+airline_names <-
+  airlines %>%
+  rename(carrier_name = name)
+
+dest_airport_names <-
+  airports %>%
+  select(dest = faa, airport_name = name)
+
+verbose_destinations_by_carrier <-
+  flights %>%
+  filter(distance < 300) %>%
+  count(carrier, dest) %>%
+  left_join(airline_names, by = "carrier") %>%
+  select(-carrier) %>%
+  left_join(dest_airport_names, by = "dest") %>%
+  select(-dest)
