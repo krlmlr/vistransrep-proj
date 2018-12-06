99a00ac0b60e2a9d9750cb84a45154fb2e8b37d9
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:36:20 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:36:20 UTC 2018

--- live/60-scales.R
+++ live/60-scales.R
@@ -13,7 +13,7 @@ mpg %>%
 
 mpg %>%
   ggplot(aes(x = displ, y = hwy, color = class)) +
-  geom_point() +
+  geom_jitter() +
   scale_x_continuous("Displacement") +
   scale_y_continuous("Highway fuel consumption\n[miles per gallon]") +
   scale_color_brewer("Car class", palette = "Set1")
