018290adb38bac8923932ad130559eaa216ec23e
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 13:14:36 +0000

    changes from cd96d1d11c38 on Thu Dec 6 13:14:36 UTC 2018

--- live/90-import.R
+++ live/90-import.R
@@ -1,6 +1,7 @@
 library(tidyverse)
+library(here)
 
 example1 <- readr::read_delim(
-  "data/example1.csv",
+  here("data/example1.csv"),
   ";", escape_double = FALSE, trim_ws = TRUE)
 View(example1)
