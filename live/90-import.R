e48cd99c889c14b045f9581f6cdc89b066eee074
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 13:15:51 +0000

    changes from cd96d1d11c38 on Thu Dec 6 13:15:51 UTC 2018

--- live/90-import.R
+++ live/90-import.R
@@ -5,3 +5,13 @@ example1 <- readr::read_delim(
   here("data/example1.csv"),
   ";", escape_double = FALSE, trim_ws = TRUE)
 View(example1)
+
+example2 <- readr::read_delim(
+  "data/example2.csv",
+  ";", escape_double = FALSE,
+  col_types = cols(
+    col1 = col_character(),
+
+                                                                    col2 = col_skip()), comment = "#",
+                       trim_ws = TRUE)
+View(example2)
