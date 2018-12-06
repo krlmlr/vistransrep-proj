6e37887896e3e6105f88264b9e7d63ba7ea20c8a
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 12:19:40 +0000

    changes from cd96d1d11c38 on Thu Dec 6 12:19:40 UTC 2018

---
+++ live/71-datameta.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(dataMeta)
+
+# example original data set for which a dictionary will be made
+data("esoph")
+my.data <- esoph
+
+# Add description for each variable names and variable type
+variable_description <- c("age group", "alcohol consumption", "tobacco consumption",
+                          "number of cases", "number of controls")
+
+variable_type <- c(0, 0, 0, 0, 0)
+
+linker <- build_linker(my.data = my.data, variable_description = variable_description,
+                       variable_type = variable_type)
+linker
+
+## Not run:
+variable_description <- c("age group", "alcohol consumption", "tobacco consumption",
+                          "number of cases", "number of controls")
+variable_type <- c(0, 2, 0, 0, 0)
+linker <- build_linker(my.data = my.data, variable_description = variable_description,
+                       variable_type = variable_type)
+linker
