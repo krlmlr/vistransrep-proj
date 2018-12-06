68abb1652204bde2e0e6f9de62827b928bab6965
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:43:53 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:43:53 UTC 2018

--- live/61-coord.R
+++ live/61-coord.R
@@ -1,2 +1,7 @@
 library(tidyverse)
 
+mpg %>%
+  ggplot(aes(x = displ, y = hwy)) +
+  geom_point() +
+  scale_x_continuous("Displacement") +
+  scale_y_continuous("Highway fuel consumption\n[miles per gallon]")
