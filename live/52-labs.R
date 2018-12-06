9cd23051c0e2b6fbb197a8635214cbbbcd1443d9
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:56:56 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:56:56 UTC 2018

--- live/52-labs.R
+++ live/52-labs.R
@@ -16,3 +16,9 @@ mpg %>%
     plot.title = element_text(hjust = 0.5),
     plot.subtitle = element_text(hjust = 0.5)
   )
+
+
+mpg %>%
+  ggplot(aes(x = displ, y = hwy, color = class)) +
+  geom_point() +
+  theme_bw()
