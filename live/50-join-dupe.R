c7eb5db75dcec3dbe3222e74fcb77ee715b74189
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:03:50 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:03:50 UTC 2018

--- live/50-join-dupe.R
+++ live/50-join-dupe.R
@@ -13,3 +13,10 @@ flights %>%
   select(1:3, carrier) %>%
   left_join(bind_rows(airlines, airlines))
 
+airlines %>%
+  count(carrier) %>%
+  filter(n > 1)
+
+flights %>%
+  select(1:3, carrier) %>%
+  left_join(airlines, by = "carrier")
