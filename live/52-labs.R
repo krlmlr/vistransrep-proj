e346e49b812c39179abadffff3f0b577ad4fd153
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:59:28 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:59:28 UTC 2018

--- live/52-labs.R
+++ live/52-labs.R
@@ -20,4 +20,22 @@ mpg %>%
 mpg %>%
   ggplot(aes(x = displ, y = hwy, color = class)) +
   geom_point() +
-  theme_bw()
+  theme_minimal()
+
+mpg %>%
+  ggplot(aes(x = displ, y = hwy, color = class)) +
+  geom_point() +
+  theme_minimal() +
+  labs(
+    x = "Displacement",
+    y = "Highway",
+    color = "Car class",
+    title = "Plot title",
+    subtitle = "Subtitle",
+    caption = "Caption",
+    tag = "Tag"
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5)
+  )
