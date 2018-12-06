7d5df41dfb81bb8ef6b95e817554d2d4f5661ccf
Kirill Müller <krlmlr@mailbox.org>
2018-12-06 10:03:17 +0000

    changes from cd96d1d11c38 on Thu Dec 6 10:03:17 UTC 2018

--- live/50-join-dupe.R
+++ live/50-join-dupe.R
@@ -2,3 +2,14 @@ library(tidyverse)
 library(nycflights13)
 
 flights
+
+airlines
+
+flights %>%
+  select(1:3, carrier) %>%
+  left_join(airlines)
+
+flights %>%
+  select(1:3, carrier) %>%
+  left_join(bind_rows(airlines, airlines))
+
