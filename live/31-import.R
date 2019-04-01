f23d2a111f516ecd1d541d9221429dc9116da8e2
Kirill Müller <kirill@cynkra.com>
2019-04-01 14:40:58 +0000

    changes from 409594ba10fa on Mon Apr 1 14:40:58 UTC 2019

--- live/31-import.R
+++ live/31-import.R
@@ -10,5 +10,8 @@ library(readr)
 example1 <-
   read_delim(
     "data/example1.csv",
-                       ";", escape_double = FALSE, trim_ws = TRUE)
+    ";",
+    escape_double = FALSE, trim_ws = TRUE
+  )
+
 View(example1)
