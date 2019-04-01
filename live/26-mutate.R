3f575bf398261c0464fa571de8e4ab880c4d6fef
Kirill Müller <kirill@cynkra.com>
2019-04-01 13:31:10 +0000

    changes from 409594ba10fa on Mon Apr 1 13:31:10 UTC 2019

--- live/26-mutate.R
+++ live/26-mutate.R
@@ -53,3 +53,11 @@ flights %>%
   mutate(lag_arr_time = lag(arr_time)) %>%
   mutate(ground_time = dep_time - lag_arr_time) %>%
   view()
+
+flights %>%
+  filter(tailnum == "N14228") %>%
+  select(year, month, day, dep_time, arr_time) %>%
+  mutate(lag_arr_time = lag(arr_time)) %>%
+  mutate(ground_time = dep_time - lag_arr_time) %>%
+  select(-lag_arr_time) %>%
+  view()
