83abf441f289b01fb162965389b35ca2d09b7a4b
Kirill Müller <kirill@cynkra.com>
2019-04-01 09:51:01 +0000

    changes from 409594ba10fa on Mon Apr 1 09:51:01 UTC 2019

---
+++ live/13-other.R
@@ -0,0 +1,76 @@
+library(tidyverse)
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+) +
+  geom_text(label = "A")
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, label = "drv")
+) +
+  geom_text()
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, label = drv)
+) +
+  geom_text()
+
+# Two layers
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, label = drv)
+) +
+  geom_point() +
+  geom_text()
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, label = drv)
+) +
+  geom_point(color = "blue") +
+  geom_text(size = 10)
+
+# Global and local aesthetic mappings
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+) +
+  geom_point(color = "blue") +
+  geom_text(
+    mapping = aes(color = class, label = drv),
+    size = 10,
+    alpha = 0.2
+  )
+
+# Good:
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+) +
+  geom_point() +
+  geom_rug()
+
+# Bad:
+ggplot(
+  data = mpg
+) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_rug(mapping = aes(x = displ, y = hwy))
+
+# Broken:
+ggplot(
+  data = mpg
+) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_rug(mapping = aes(x = displ, y = cty))
+
+# Smooth
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+) +
+  geom_point() +
+  geom_smooth()
