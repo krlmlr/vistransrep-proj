9f0dfa3f5f93e4e250ec8f445c78ad2b8faf91f8
Kirill Müller <kirill@cynkra.com>
2019-04-01 14:11:44 +0000

    changes from 409594ba10fa on Mon Apr 1 14:11:44 UTC 2019

--- live/27-summarize.R
+++ live/27-summarize.R
@@ -47,8 +47,22 @@ flights %>%
   )
 
 flights %>%
-  group_by(year, month, day) %>% I
+  group_by(year, month, day) %>%
   summarize(
     mean_air_time = mean(air_time, na.rm = TRUE),
     median_air_time = median(air_time, na.rm = TRUE)
   )
+
+flights %>%
+  group_by(origin) %>%
+  summarize(
+    mean_air_time = mean(air_time, na.rm = TRUE),
+    median_air_time = median(air_time, na.rm = TRUE)
+  )
+
+flights %>%
+  group_by(year, month, day) %>%
+  summarize(
+  mean_air_time = mean(air_time, na.rm = TRUE),
+  median_air_time = median(air_time, na.rm = TRUE)
+)
