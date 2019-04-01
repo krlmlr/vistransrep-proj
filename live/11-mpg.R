dd7034e618439096bce502b9e81f74addc450bd1
Kirill Müller <kirill@cynkra.com>
2019-04-01 08:52:12 +0000

    changes from d70b2ceb7a2e on Mon Apr 1 08:52:12 UTC 2019

--- live/11-mpg.R
+++ live/11-mpg.R
@@ -3,6 +3,12 @@ library(tidyverse)
 # # One time:
 # install.packages("tidyverse")
 
+# Empty graph
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+)
+
 ggplot(
   data = mpg,
   mapping = aes(x = displ, y = hwy)
