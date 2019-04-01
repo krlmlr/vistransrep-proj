ee09fffc61ce0d258c8203f20cc46b7c0cf6d532
Kirill Müller <kirill@cynkra.com>
2019-04-01 11:41:30 +0000

    changes from 409594ba10fa on Mon Apr 1 11:41:30 UTC 2019

--- live/21-conflicted.R
+++ live/21-conflicted.R
@@ -1,1 +1,12 @@
 library(tidyverse)
+library(conflicted)
+conflict_prefer("filter", "dplyr")
+
+my_mpg <- mpg
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+) +
+  geom_point() +
+  facet_grid(year ~ drv)
