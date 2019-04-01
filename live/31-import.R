a8f99a1a9d2c7b31bf93294da448cd747e33a01d
Kirill Müller <kirill@cynkra.com>
2019-04-01 14:40:50 +0000

    changes from 409594ba10fa on Mon Apr 1 14:40:50 UTC 2019

--- live/31-import.R
+++ live/31-import.R
@@ -6,3 +6,9 @@ conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 
 
+library(readr)
+example1 <-
+  read_delim(
+    "data/example1.csv",
+                       ";", escape_double = FALSE, trim_ws = TRUE)
+View(example1)
