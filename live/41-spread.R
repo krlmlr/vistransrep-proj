ee2359121f8ccc847a58c08fe170fa6a4716b6f6
Kirill Müller <kirill@cynkra.com>
2019-04-02 08:10:04 +0000

    changes from 37c05089f7f3 on Tue Apr 2 08:10:04 UTC 2019

--- live/41-spread.R
+++ live/41-spread.R
@@ -6,3 +6,12 @@ conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 
 
+table1
+table2
+
+table1 %>%
+  group_by(country) %>%
+  summarize(
+    max_cases = max(cases),
+    max_population = max(population)
+  )
