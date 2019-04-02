3e35b4a790e538881841241582af4352a7658ce1
Kirill Müller <kirill@cynkra.com>
2019-04-02 19:06:48 +0000

    changes from 37c05089f7f3 on Tue Apr 2 19:06:48 UTC 2019

--- live/71-tidy-walk.R
+++ live/71-tidy-walk.R
@@ -7,5 +7,17 @@ conflict_prefer("lag", "dplyr")
 
 
 iris %>%
-  slice(1:3) %>% I
-  with(walk2(Sepal.Width, Sepal.Length, cat))
+  slice(1:3) %>%
+  with(walk2(Sepal.Width, Sepal.Length, ~ cat(..., "\n"))) %>%
+  count()
+
+
+x <- walk2(iris$Sepal.Width[1:3], iris$Sepal.Length[1:3], ~ cat(..., "\n"))
+x
+
+iris %>%
+  slice(1:3) %>%
+  select(Sepal.Width, Sepal.Length) %>%
+  pwalk(~ cat(..., "\n")) %>%
+  count()
+
