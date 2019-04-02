b9be2fa89a2275f0871c96f4fdb4428e2bad7c6a
Kirill Müller <kirill@cynkra.com>
2019-04-02 09:34:04 +0000

    changes from 37c05089f7f3 on Tue Apr 2 09:34:04 UTC 2019

--- live/28-multiplot.R
+++ live/28-multiplot.R
@@ -6,15 +6,18 @@ conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 
 
+# Bar plot: flights per carrier
 flights %>%
   ggplot(aes(x = carrier)) +
   geom_bar()
 
+# Bar plot: flights per carrier, one facet per month
 flights %>%
   ggplot(aes(x = carrier)) +
   geom_bar() +
   facet_wrap(~month)
 
+# Extract a function
 plot_fun <- function(data) {
   data %>%
     ggplot(aes(x = carrier)) +
@@ -24,6 +27,7 @@ plot_fun <- function(data) {
 
 plot_fun(flights)
 
+# Call this function for each subgroup
 flights %>%
   group_by(month) %>%
   summarize(
